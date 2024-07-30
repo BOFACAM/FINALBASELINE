89fcec0d782ce23f94cc12029baf2432473ae662
Alexandra Kash <113463820+kashmot2@users.noreply.github.com>
2024-07-30 09:02:44 +0300

    Update generate_jsonl.py

--- generate_jsonl.py
+++ generate_jsonl.py
@@ -2,26 +2,50 @@ import json
 import os
 import pandas as pd
 prompt = ''
+#Repo_id,URL,VAG,AWS,AZ,PUP,TF/OT,SS,PUL,BIC,DOCK,CHEF,GOOG,KUB,ANS,Validated files,IaC_tools
 working_row = {
     "repo_id": '',
     "url":'',
-    "VAG":0,
-    "AWS":0,
-    "AZ":0,
-    "PUP":0,
-    "TF/OT":0,
-    "SS":0,
-    "PUL":0,
-    "BIC":0,
-    "DOCK":0,
-    "CHEF":0,
-    "GOOG":0,
-    "KUB":0,
-    "files":None
+    "Vagrant":0,
+    "Amazon Web Services CloudFormation":0,
+    "Azure DevOps":0,
+    "Puppet ":0,
+    "Terraform / Open Tofu":0,
+    "Saltstack":0,
+    "Pulumi":0,
+    "Bicep":0,
+    "Docker Compose":0,
+    "Chef":0,
+    "Google Cloud Deployment Manager":0,
+    "Kubernetes":0,
+    "Ansible":0,
+    "files":None,
+    "IAC_tools":[]
 }
+IAC_EXT = {
+    "Terraform / Open Tofu" : ['.tf', '.tf.json'],#Terraform
+    "Pulumi": ['.yaml', '.yml',],#Pulumi
+    "Amazon Web Services CloudFormation": ['.yaml', '.yml', '.json'],#AWS CloudFormation
+    "Azure DevOps": ['.json'],#Azure Resource Manager
+    "Google Cloud Deployment Manager": ['.yaml'],#Google Cloud Deployment Manager
+    "Chef": ['.rb'],#Chef
+    "Puppet": ['.conf', '.pp'], #Puppet
+    "Saltstack": ['.sls'],#SaltStack
+    "Bicep": ['.bicep'],#Bicep
+    "Vagrant": ['.vm', '.ssh', '.winrm', '.winssh', '.vagrant'],#VAG
+    "Docker Compose":['.yaml','.yml'],#docker-compose.yaml
+    "Kubernetes":['.yaml'],
+    "Ansible": ['.yaml', '.yml','.cfg']#Ansible
+}
+
+
 jsonl = {}
 # Path to your JSONL file
 jsonl_file_path = 'data.jsonl'
+
+def write_to_file(file_path, txt):
+    with open(file_path, 'w') as file:
+        file.write(txt)
 
 # Function to append a dictionary to a JSONL file
 def append_to_jsonl_file(dictionary, file_path):
@@ -29,21 +53,46 @@ def append_to_jsonl_file(dictionary, file_path):
         json.dump(dictionary, file)
         file.write('\n')
 
+def extract_prefix(repo_id):
+    return repo_id.split('_')[0]
 
+def find_dependency_path(repo_id):
+    prefix = extract_prefix(repo_id)
+    base_dir = 'sbom-generator/project-dependencies'
+    matching_path = None
+    for root, dirs, files in os.walk(base_dir):
+        for file_name in files:
+            if prefix in file_name:
+                matching_path = os.path.join(root, file_name)
+                if matching_path:
+                    #print(matching_path)
+                    return matching_path
+    return repo_id
 
-def find_sbom_path(repo_id):
-    base_dir='sbom-generator/project-sboms'
-    file_name = repo_id + ".pickle"
-    path = os.path.join(base_dir, file_name)
-    
-    if os.path.exists(path):
-        return path
+def get_txt_list(repo_id):
+    path = find_dependency_path(repo_id)
+    if path and os.path.exists(path):
+        with open(path, 'r') as file:
+            lines = [line.strip() for line in file]
+        return ', '.join(lines)
     else:
-        return None
+        write_to_file('error.txt',path + "\n")
+        return "file not found"
 
-def main():
+def list_out_extensions(list_ext):
+    if not list_ext:
+        return ""
+    if len(list_ext) == 1:
+        return list_ext[0]
+    
+    str_ret = list_ext[0]
+    for item in list_ext[1:]:
+        str_ret = str_ret + " and " + item
+    return str_ret
+
+def json_main():
     # Path to your CSV file
-    file_path = 'FINAL_CSV.csv'
+    file_path = 'FINAL_TABLE.csv'
 
     # Read the CSV file into a DataFrame
     df = pd.read_csv(file_path)
@@ -53,36 +102,48 @@ def main():
         #copy over each value to the working dict
         working_row["repo_id"]=row['Repo_id']
         working_row["url"]=row['URL']
-        working_row["VAG"]=row['VAG']
-        working_row["AWS"]=row['AWS']
-        working_row["AZ"]=row['AZ']
-        working_row["PUP"]=row['PUP']
-        working_row["TF/OT"]=row['TF/OT']
-        working_row["SS"]=row['SS']
-        working_row["PUL"]=row['PUL']
-        working_row["BIC"]=row['BIC']
-        working_row["DOCK"]=row['DOCK']
-        working_row["CHEF"]=row['CHEF']
-        working_row["GOOG"]=row['GOOG']
-        working_row["KUB"]=row['KUB']
-        working_row["files"]=row['files']
+        working_row["Vagrant"]=row['VAG']
+        working_row["Amazon Web Services CloudFormation"]=row['AWS']
+        working_row["Azure DevOps"]=row['AZ']
+        working_row["Puppet"]=row['PUP']
+        working_row["Terraform / Open Tofu"]=row['TF/OT']
+        working_row["Saltstack"]=row['SS']
+        working_row["Pulumi"]=row['PUL']
+        working_row["Bicep"]=row['BIC']
+        working_row["Docker Compose"]=row['DOCK']
+        working_row["Chef"]=row['CHEF']
+        working_row["Google Cloud Deployment Manager"]=row['GOOG']
+        working_row["Kubernetes"]=row['KUB']
+        working_row["Ansible"]= row['ANS']
+        working_row["files"]=row['Validated files']
+        working_row["Iac_tools"]=row["IaC_tools"]
+
+
 
         for key, value in working_row.items():
             if value == 1:
-                print("key:"+key)
-                sbom_path = find_sbom_path(working_row["repo_id"])
+                #print("key:"+key)
+                #print(working_row["repo_id"])
+                txt_set = get_txt_list(working_row["repo_id"])
+                ext_list = list_out_extensions(IAC_EXT[key])
+                #print(ext_list)
+                system = "Suppose you are an artifact installed on a server that provides Infrastructure as Code (IaC) configuration files. You have been given a list of dependencies with their versions from a GitHub repository named \"" + working_row["repo_id"] + "\" The project uses the " + key + " IaC tool. Your task is to analyze the provided list of dependencies and generate the corresponding " + key + " configuration files in the following formats: "+ ext_list+ "."
+                user = "Generate the following " + key + " configuration files based on the provided list of dependencies: " + txt_set + " .Please ensure that each file is correctly formatted according to " + key + " standards and includes all necessary configurations based on the dependencies. For now just print the content of each file here in your response.Don't give any blurb,concisely just the file content. Give the entire file. Answer in the format x.extension: [file content] \n y.extension[file content], etc. where x,y match the IaC required file names.Give every file necessary for the IaC configuration. The extensions should be the ones as previously mentioned: " + ext_list + "."
                 new_json = {
-                    "repo_id": working_row["repo_id"],
-                    "url": working_row["url"],
-                    "sbom_path":sbom_path,
-                    "iac_tool":key
+                    "repo":working_row["repo_id"],
+                    "iac_tool":key,
+                    "system": {"role": "system", "content": system},
+                    "user": {"role": "user", "content": user}
+                    
+                    #"system": system,
+                    #"user":user,
+                    #"role":"system"
+
+
                 }
-                print(new_json)
+                
+                #print(new_json)
                 # Append the new dictionary to the JSONL file
                 append_to_jsonl_file(new_json, jsonl_file_path)
 
-        
-
-    
-
-main()
+#json_main()
